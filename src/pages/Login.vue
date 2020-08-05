11e65ee03c86adfd33741adac75d3f8b3b83a1ef
charly <charlygame@126.com>
2020-08-05 23:55:11 +0800

    添加登录界面

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,25 +1,39 @@
 <template>
-    <a-form>
+    <div class="login">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <a-form :form="form" :label-col="{span: 6, offset: 4}" :wrapper-col="{span: 4}">
         <a-form-item label="用户名">
             <a-input id="userName" />
         </a-form-item>
         <a-form-item label="密码">
             <a-input id="password" />
         </a-form-item>
-        <a-form-item>
-            <a-button type="primary">
-                登录
-            </a-button>
+        <a-form-item :label-col="{span: 6, offset: 4}" :wrapper-col="{span: 24}">
+            <div class="buttonGroup">
+                <a-button type="primary">
+                    登录
+                </a-button>
+                
+                <a-button type="primary">
+                    注册
+                </a-button>
+            </div>
         </a-form-item>
     </a-form>
+    </div>
 </template>
 <script>
+import {Form, Input} from 'ant-design-vue'
+import Vue from 'vue'
+
+Vue.use(Form);
+Vue.use(Input);
 
 export default {
     name: 'Login',
     data() {
         return {
-
+            form: ''
         }
     },
     methods: {
@@ -28,3 +42,10 @@ export default {
 
 }
 </script>
+<style>
+    /* .buttonGroup {
+        width: 400px;
+        display: flex;
+        justify-content: center;
+    } */
+</style>
